5c87425b1a39b8ce03a909a7b95e4d699e329d70
muhamadwildan99 <wildanfadilah669@gmail.com>
2024-11-26 01:59:19 +0700

    Add files via upload

---
+++ Katalog Makanan/Model/data.js
@@ -0,0 +1,28 @@
+const model = {
+  items: [
+    {
+      name: "Nasi Goreng",
+      price: 25000,
+      description: "Nasi goreng dengan bumbu rempah istimewa.",
+      image: "./assets/images/makanan1.jpg",
+    },
+    {
+      name: "Sate Ayam",
+      price: 30000,
+      description: "Sate ayam dengan saus kacang gurih.",
+      image: "./assets/images/makanan2.jpg",
+    },
+    {
+      name: "Mie Goreng",
+      price: 20000,
+      description: "Mie goreng pedas manis dengan ayam suwir.",
+      image: "./assets/images/makanan3.jpg",
+    },
+    {
+      name: "Bakso",
+      price: 18000,
+      description: "Bakso daging sapi dengan kuah segar.",
+      image: "./assets/images/makanan4.jpg",
+    },
+  ],
+};
